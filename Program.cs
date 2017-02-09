502b7b2c47b2ddb6c1bb4d7eb30c98e565b414d8
Altynaay <yerkhassym.altynay@gmail.com>
2017-02-09 21:56:17 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,35 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Student
+namespace cursor
 {
     class Program
     {
-        public class Student
+        static void ShowInfo(DirectoryInfo directory, int cursor) //метод, который выводит информацию об объекте
         {
-            public string name;
-            public string surname;
-            public double gpa;
-
-            public Student(string name, string surname, double gpa)
+            Console.BackgroundColor = ConsoleColor.Green; //меняет цвет фона текста
+            int index = 0;
+            foreach (FileSystemInfo fInfo in directory.GetFileSystemInfos()) //для каждого файла и папки 
             {
-                this.name = name;
-                this.surname = surname;
-                this.gpa = gpa;
+                if (index == cursor)
+                    Console.ForegroundColor = ConsoleColor.Red; //меняет цвет самого текста
+                index++;
+                if (fInfo.GetType() == typeof(FileInfo)) //определяет тип объекта , файл или папка
+                    Console.Write("File: ");
+                else
+                    Console.Write("Directory: ");
+                Console.WriteLine(fInfo.Name);
             }
-            public void Write()
-            {
-                return this.name + " " + this.surname + " " + this.gpa;
-            }
+                    
         }
-        public static void Main(string[] args)
-        {
-            Student a = new Student("Aisultan", "Akhzhan", 3.0);
-            Student b = new Student("Arsen", "Zhumashev", 3.55);
-            Console.WriteLine(a.Write());
-            Console.ReadKey();
 
+        static void Main(string[] args)
+        {
+            int cursor = 0;
+            DirectoryInfo directory = new DirectoryInfo(@"C:\DELL"); //создает новый экземпляр класса DirectoryInfo
+            while(true)
+            {
+                Console.Clear(); //очищает окно консоли 
+                ShowInfo(directory, cursor);
+                ConsoleKeyInfo pressedKey = Console.ReadKey(); //описывает функцию нажатой клавиши
+                if (pressedKey.Key == ConsoleKey.UpArrow)
+                    if (cursor > 0)
+                        cursor--;
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                    if (cursor < directory.GetFileSystemInfos().Length - 1)
+                        cursor++;
+                if (pressedKey.Key == ConsoleKey.Enter)
+                {
+                    FileSystemInfo fs = directory.GetFileSystemInfos()[cursor];
+                    if (fs.GetType() == typeof(DirectoryInfo)) 
+                    {
+                        directory = new DirectoryInfo(fs.FullName); //создает новый экземпляр класса diretoryinfo
+                    }
+                    else
+                    {
+                        try // пытается выполнить прописанные операции 
+                        {
+                            Console.Clear();
+                            StreamReader sr = new StreamReader(fs.FullName); 
+                            Console.Write(sr.ReadToEnd());
+                            Console.ReadKey(); 
+                            sr.Close();
+                        }
+
+                        catch (Exception e) //при невыполнении выше прописанных операций
+                        {
+                            Console.WriteLine("error");
+                        }
+                    }
+                    break;
+                }
+                if (pressedKey.Key == ConsoleKey.Backspace)
+                {
+                    try
+                    {
+                        directory = Directory.GetParent(directory.FullName); //возвращается в начальную папку 
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+                if (pressedKey.Key == ConsoleKey.Escape)
+                    break;
+            }
         }
     }
 }
